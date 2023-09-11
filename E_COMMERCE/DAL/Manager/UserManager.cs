9b2efc7545e618b24f9c3445ee02363993c836a8
AmalPB15 <139753642+AmalPB15@users.noreply.github.com>
2023-09-11 16:49:23 +0530

    Insert,Update-Product

--- E_COMMERCE/DAL/Manager/UserManager.cs
+++ E_COMMERCE/DAL/Manager/UserManager.cs
@@ -6,11 +6,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DAL.Manager
 {
     public class UserManager
     {
-        E_COMMERCEEntities dbhelper = new E_COMMERCEEntities();
+        Model1 dbhelper = new Model1();
         public string AddUser(USER usr)
         {
             int result = 0;
@@ -27,7 +28,6 @@ namespace DAL.Manager
                 return "Error ";
             }
         }
-
         public USER userDetails(int Id)
         {
             USER return_Obj = new USER();
@@ -37,9 +37,6 @@ namespace DAL.Manager
         {
             return dbhelper.USERs.Where(p => p.USER_ID == id).ToList();
         }
-       
-        
-
         public USER UserLogin(USER ur)
         {
             var log = dbhelper.USERs.Where(x => x.USER_EMAIL.Equals(ur.USER_EMAIL) &&
@@ -50,10 +47,56 @@ namespace DAL.Manager
 
 
 
+        //Insert
+        public string productInsert(PRODUCT product)
+        {
+            int result = 0;
+            var objProduct=dbhelper.PRODUCTS.Where(e=>e.PRO_ID == product.PRO_ID && e.PRO_STATUS!="D").SingleOrDefault();
+            if (objProduct == null)
+            {
+                product.PRO_STATUS = "A";
+                dbhelper.PRODUCTS.Add(product);
+                result = dbhelper.SaveChanges();
 
+                return "Product Inserted Successfully";
+            }
+            else
+            {
+                return "Cannot be Inserted";
+            }
+        }
+
+        //Update
+        public string productUpdate(PRODUCT prd)
+        {
+            int result = 0;
+            var objProduct = dbhelper.PRODUCTS.Where(e => e.PRO_ID == prd.PRO_ID && e.PRO_STATUS != "D").FirstOrDefault();
+            //PRODUCT pro = product;
+
+            if (objProduct != null)
+            {
+                objProduct.PRO_NAME = prd.PRO_NAME;
+                objProduct.PROCAT_ID= prd.PROCAT_ID;
+                objProduct.PRO_DESC = prd.PRO_DESC;
+                objProduct.PRO_STOCK = prd.PRO_STOCK;
+                objProduct.PRO_IMAGE = prd.PRO_IMAGE;
+                objProduct.PRO_PRICE = prd.PRO_PRICE;
+                objProduct.PRO_STATUS = "A";
+                objProduct.PRO_MODIBY = "Admin";
+                objProduct.PRO_MODIDATE = DateTime.Now.ToString();
+
+                dbhelper.Entry(objProduct).State = EntityState.Modified;
+                result = dbhelper.SaveChanges();
+
+                return "Product Updated Successfully.";
+
+            }
+            else
+            {
+                return "Product not found.";
+            }
+        }
 
       
-
     }
-
 }
